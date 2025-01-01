52a3b79795fc0d88aca671c99da706737ad9428b
AbbasKazmi1214 <syedmohammedabbaskazmi832@gmail.com>
2025-01-01 21:06:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
-from sklearn.datasets import load_diabetes,load_boston
+from sklearn.datasets import load_boston
 
 st.set_page_config(page_title="Machine Learning App",
                    layout="wide")
